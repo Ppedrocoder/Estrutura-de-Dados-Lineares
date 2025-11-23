6907ebd671d272a4dacdc60ead3682ea2dc0e919
Pedro Ricardo <rpedroricardo87@gmail.com>
2025-11-22 21:10:34 -0300

    criando arvore binaria

---
+++ ArvoreBinaria/ArvoreBinaria.java
@@ -0,0 +1,13 @@
+package ArvoreBinaria;
+
+public class ArvoreBinaria {
+    private No root;
+    private int size;
+    public ArvoreBinaria(Object o) {
+        root = new No(null, o);
+        this.size = 1;
+    }
+    public No getRoot() {
+        return this.root;
+    }
+}
---
+++ ArvoreBinaria/No.java
@@ -0,0 +1,44 @@
+package ArvoreBinaria;
+
+public class No {
+    private Object element;
+    private No parent;
+    private No left_child;
+    private No right_child;
+    public No() {
+        parent = null;
+        left_child = null;
+        right_child = null;
+        element = null;
+    }
+    public No(No Parent,Object element) {
+        this.element = element;
+        parent = Parent;
+        left_child = null;
+        right_child = null;
+    }
+    public void setElement(Object element) {
+        this.element = element;
+    }
+    public void setParent(No parent) {
+        this.parent = parent;
+    }
+    public void setLeftChild(No left_child) {
+        this.left_child = left_child;
+    }
+    public void setRightChild(No right_child) {
+        this.right_child = right_child;
+    }
+    public Object getElement() {
+        return element;
+    }
+    public No getParent() {
+        return parent;
+    }
+    public No getLeftChild() {
+        return left_child;
+    }
+    public No getRightChild() {
+        return right_child;
+    }
+}
